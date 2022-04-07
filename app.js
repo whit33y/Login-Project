866dea5cccd1ed29a453100d35d5840c16f042b5
whit33y <troppout@gmail.com>
2022-04-07 15:58:05 +0200

    New views, ejs and basic forms

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ const app = express();
 app.use(express.urlencoded());
 
 const mongoose = require('mongoose');
-
 mongoose.connect('mongodb://localhost:27017/login-project')
     .then(()=>{
         console.log('Mongo successfully connected')
     }).catch(err=>{
         console.log('Mongo connection error =>',err)
-    })
+    });
+
+const ejsMate = require('ejs-mate'); 
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
 
 const port = 3000
 app.listen(port, ()=>{
@@ -18,5 +21,11 @@ app.listen(port, ()=>{
 
 
 app.get('/', (req,res)=>{
-    res.send('Hello world!')
+    res.render('home')
+})
+app.get('/login', (req,res)=>{
+    res.render('login/login')
+})
+app.get('/reg', (req,res)=>{
+    res.render('login/register')
 })
